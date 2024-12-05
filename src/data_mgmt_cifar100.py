d228491e85c472913680f63f6aba53c5644dbcf0
Adrian Duric <adria.dur@gmail.com>
2024-12-05 14:56:16 +0100

    closer to cifar 10 support

--- src/data_mgmt_cifar100.py
+++ src/data_mgmt_cifar100.py
@@ -125,56 +125,73 @@ LABEL_TO_INDEX_CIFAR100 = {
 }
 
 
-def save_cifar100_in_directory(data_root):
+def save_cifar_in_directory(data_root):
     """
-    Processes the CIFAR-100 dataset:
-        - Downloads the dataset if not already present
-        - Organizes images into directories based on coarse and fine labels
+    Processes the CIFAR-10 and CIFAR-100 datasets:
+        - Downloads the datasets if not already present
+        - Organizes images into directories based on labels
         - Saves the images in '.jpg' format
         - Creates a CSV file with details of the images
     
     Args:
-        data_root (str): The root directory where the CIFAR-100 dataset will be stored and processed
+        data_root (str): The root directory where the CIFAR datasets will be stored and processed
     """
     
-    def load_coarse_labels(file):
+    def load_labels(file, cifar_version):
         """
-        Loads coarse labels from CIFAR-100 dataset files
+        Loads labels from CIFAR dataset files
         
         Args:
-            file (str): Path to the CIFAR-100 dataset file
+            file (str): Path to the CIFAR dataset file
+            cifar_version (int): The version of CIFAR dataset (10 or 100)
         
         Returns:
-            list: List of coarse labels
+            list: List of labels
         """
         with open(file, 'rb') as f:
             data = pickle.load(f, encoding='latin1')
-            return data['coarse_labels']
+            if cifar_version == 100:
+                return data['coarse_labels'], data['fine_labels']
+            else:
+                return data['labels']
 
-    def save_images(dataset, directory, coarse_labels, set_name, csv_writer):
+    def save_images(dataset, directory, labels, set_name, csv_writer, cifar_version):
         """
-        Saves images from the CIFAR-100 dataset into organized directories and writes details to CSV
-        
+        Saves images from the CIFAR dataset into organized directories and writes details to CSV
+
         Args:
-            dataset (Dataset): The CIFAR-100 dataset
-            directory (str): The root directory where images will be saved
-            coarse_labels (list): List of coarse labels corresponding to the dataset
-            set_name (str): The name of the set ('train' or 'test')
-            csv_writer (csv.writer): The CSV writer object for logging image details
+            dataset (Dataset): The CIFAR dataset (CIFAR-10 or CIFAR-100).
+            directory (str): The root directory where images will be saved.
+            labels (tuple or list): List of labels corresponding to the dataset. If CIFAR-100, 
+                                    it's a tuple containing two lists (coarse labels and fine labels).
+            set_name (str): The name of the set ('train' or 'test').
+            csv_writer (csv.writer): The CSV writer object for logging image details.
+            cifar_version (int): The version of CIFAR dataset (10 or 100).
         """
+
+        # Get all class names in the dataset
+        if cifar_version == 100:
+            with open(os.path.join(dataset.root, 'cifar-100-python', 'meta'), 'rb') as f:
+                meta = pickle.load(f, encoding='latin1')
+                fine_label_names = meta['fine_label_names']
+                coarse_label_names = meta['coarse_label_names']
+        else:
+            fine_label_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
         
         print(f"Saving images to {directory}")
-        for idx, (img, fine_label) in tqdm(enumerate(dataset)):
+        for idx, (img, label) in tqdm(enumerate(dataset)):
         
-            # Get the coarse label for the current image
-            coarse_label = coarse_labels[idx]
-
-            # Get the class names
-            fine_class_name = fine_label_names[fine_label]
-            coarse_class_name = coarse_label_names[coarse_label]
-        
-            # Create a subdirectory for each superclass and class
-            class_dir = os.path.join(directory, coarse_class_name, fine_class_name)
+            # Get the class names and create a subdirectory for each superclass and class
+            if cifar_version == 100:
+                coarse_label = labels[0][idx]
+                fine_label = labels[1][idx]
+                coarse_class_name = coarse_label_names[coarse_label]
+                fine_class_name = fine_label_names[fine_label]
+                class_dir = os.path.join(directory, coarse_class_name, fine_class_name)
+            else:
+                fine_label = label
+                fine_class_name = fine_label_names[fine_label]
+                class_dir = os.path.join(directory, fine_class_name)
             os.makedirs(class_dir, exist_ok=True)
         
             # Save the image
@@ -183,43 +200,57 @@ def save_cifar100_in_directory(data_root):
             img.save(img_path, 'JPEG')
 
             # Write to CSV
-            csv_writer.writerow([set_name, coarse_class_name, fine_class_name, img_name])
+            if cifar_version == 100:
+                csv_writer.writerow([set_name, coarse_class_name, fine_class_name, img_name])
+            else:
+                csv_writer.writerow([set_name, fine_class_name, img_name])
 
-    # Load the CIFAR-100 dataset
-    train_dataset = datasets.CIFAR100(root=data_root, train=True, download=True)
-    test_dataset = datasets.CIFAR100(root=data_root, train=False, download=True)
+    def process_cifar(cifar_version):
+        """
+        Processes and saves the CIFAR dataset based on the version (10 or 100)
 
-    # Load meta file to get class names
-    with open(os.path.join(train_dataset.root, 'cifar-100-python', 'meta'), 'rb') as f:
-        meta = pickle.load(f, encoding='latin1')
-        fine_label_names = meta['fine_label_names']
-        coarse_label_names = meta['coarse_label_names']
+        Args:
+            cifar_version (int): The version of CIFAR dataset (10 or 100)
+        """
 
-    # Load coarse labels for training and test sets
-    train_coarse_labels = load_coarse_labels(os.path.join(train_dataset.root, 'cifar-100-python', 'train'))
-    test_coarse_labels = load_coarse_labels(os.path.join(train_dataset.root, 'cifar-100-python', 'test'))
+        # Load the CIFAR dataset
+        if cifar_version == 100:
+            dataset_func = datasets.CIFAR100
+        else:
+            dataset_func = datasets.CIFAR10
+        
+        train_dataset = dataset_func(root=data_root, train=True, download=True)
+        test_dataset = dataset_func(root=data_root, train=False, download=True)
 
-    # Create directories for storing images
-    train_dir = os.path.join(data_root, "cifar-100-python/cifar-100_train")
-    test_dir = os.path.join(data_root, "cifar-100-python/cifar-100_test")
-    os.makedirs(train_dir, exist_ok=True)
-    os.makedirs(test_dir, exist_ok=True)
+        # Load labels for training and test sets
+        if cifar_version == 100:
+            train_labels = load_labels(os.path.join(train_dataset.root, 'cifar-100-python', 'train'), cifar_version)
+            test_labels = load_labels(os.path.join(train_dataset.root, 'cifar-100-python', 'test'), cifar_version)
+        else:
+            train_labels = load_labels(os.path.join(train_dataset.root, 'cifar-10-batches-py', 'data_batch_1'), cifar_version)
+            test_labels = load_labels(os.path.join(train_dataset.root, 'cifar-10-batches-py', 'test_batch'), cifar_version)
 
-    # Open CSV file for writing
-    csv_file_path = os.path.join(data_root, 'cifar-100-python/image-labels.csv')
-    with open(csv_file_path, 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(['Set (train or test)', 'Coarse Label', 'Fine Label', 'File Name'])
-    
-        # Save training images and write to CSV
-        save_images(train_dataset, train_dir, train_coarse_labels, 'train', csv_writer)
-    
-        # Save test images and write to CSV
-        save_images(test_dataset, test_dir, test_coarse_labels, 'test', csv_writer)
+        # Create directories for storing images
+        train_dir = os.path.join(data_root, f"cifar-{cifar_version}-python/cifar-{cifar_version}_train")
+        test_dir = os.path.join(data_root, f"cifar-{cifar_version}-python/cifar-{cifar_version}_test")
+        os.makedirs(train_dir, exist_ok=True)
+        os.makedirs(test_dir, exist_ok=True)
 
-    print("Images have been saved and CSV file has been created successfully.")
+        csv_file_path = os.path.join(data_root, f'cifar-{cifar_version}-python/image-labels.csv')
+        with open(csv_file_path, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            if cifar_version == 100:
+                csv_writer.writerow(['Set (train or test)', 'Coarse Label', 'Fine Label', 'File Name'])
+            else:
+                csv_writer.writerow(['Set (train or test)', 'Fine Label', 'File Name'])
+            save_images(train_dataset, train_dir, train_labels, 'train', csv_writer, cifar_version)
+            save_images(test_dataset, test_dir, test_labels, 'test', csv_writer, cifar_version)
 
+        print(f"CIFAR-{cifar_version} images have been saved and CSV file has been created successfully.")
 
+    # Process and save both CIFAR-10 and CIFAR-100 datasets
+    process_cifar(10)
+    process_cifar(100)
 
 
 # Data transforms to be used for training and evaluation of models
@@ -262,9 +293,9 @@ data_transforms = {
 }
 
 
-class CIFAR100Dataset(Dataset):
+class CIFARDataset(Dataset):
     """
-    Custom PyTorch Dataset object for the CIFAR-100 dataset.
+    Custom PyTorch Dataset object for the CIFAR dataset.
 
     Attributes:
         paths (list): Stores file paths to each image in the dataset.
@@ -275,7 +306,7 @@ class CIFAR100Dataset(Dataset):
 
     def __init__(self, paths, labels, transform_type, augmented_data):
         """
-        Initializes the CIFAR100Dataset instance.
+        Initializes the CIFARDataset instance.
 
         Args:
             paths (list): List of file paths to the images.
@@ -329,25 +360,26 @@ class CIFAR100Dataset(Dataset):
         return image, label, path, idx
 
 
-def prepare_data_cifar100(seed, augmented_data, model_explanation, split, batch_size=params["batch_size"]):
+def prepare_data_cifar(seed, augmented_data, model_explanation, split, cifar_version, batch_size=params["batch_size"]):
     """
-    Processes the CIFAR-100 dataset to return PyTorch Dataset and DataLoader objects.
+    Processes the CIFAR dataset to return PyTorch Dataset and DataLoader objects.
 
     Args:
         seed (int): Seed for random number generation to ensure reproducibility.
         augmented_data (bool): Indicates whether the dataset consists of augmented images with explanations.
         model_explanation (str): Name of the model providing explanations, e.g., 'densenet161'.
         split (bool): If True, splits Datasets and DataLoader objects into training, validation, and test sets. 
-                      If False, returns one Dataset and one DataLoader object containing all images from CIFAR-100.
+                      If False, returns one Dataset and one DataLoader object containing all images from CIFAR.
+        cifar_version (int): The version of CIFAR dataset (10 or 100)
         batch_size (int): Batch size for the DataLoader.
 
     Returns:
         If `split` is True:
-            Tuple[CIFAR100Dataset, CIFAR100Dataset, CIFAR100Dataset, DataLoader, DataLoader, DataLoader]:
+            Tuple[CIFARDataset, CIFARDataset, CIFARDataset, DataLoader, DataLoader, DataLoader]:
             train_dataset, val_dataset, test_dataset, train_dataloader, val_dataloader, test_dataloader
         
         If `split` is False:
-            Tuple[CIFAR100Dataset, DataLoader]: complete_dataset, complete_dataloader
+            Tuple[CIFARDataset, DataLoader]: complete_dataset, complete_dataloader
     """
 
     # Setting seed as specified
@@ -356,12 +388,12 @@ def prepare_data_cifar100(seed, augmented_data, model_explanation, split, batch_
 
     # Storing paths to images and corresponding labels
     if augmented_data:
-        img_dir_path = os.path.join(os.getcwd(), f"res/augmented_images/cifar-100-python/{model_explanation}")
-        csv_file_path = os.path.join(os.getcwd(), "res/augmented_images/cifar-100-python")
+        img_dir_path = os.path.join(os.getcwd(), f"res/augmented_images/cifar-{cifar_version}-python/{model_explanation}")
+        csv_file_path = os.path.join(os.getcwd(), f"res/augmented_images/cifar-{cifar_version}-python")
     else:
-        img_dir_path = os.path.join(os.getcwd(), "res/cifar-100-python")
+        img_dir_path = os.path.join(os.getcwd(), f"res/cifar-{cifar_version}-python")
 
-    # File handling for CIFAR-100 data
+    # File handling for CIFAR data
     all_paths = []
     all_labels = []
     train_val_paths = []
@@ -377,41 +409,53 @@ def prepare_data_cifar100(seed, augmented_data, model_explanation, split, batch_
 
     for line in file:
 
-        set_type, coarse_label, fine_label, file_name = line.strip().split(",")
-
-        file_name += ".pt" if augmented_data else ".jpg"
-        set_type = f"cifar-100_{set_type}"
-
-        path_to_img_file = os.path.join(
-            img_dir_path, set_type, coarse_label, fine_label, file_name
-        )
-
-        enumerated_label = LABEL_TO_INDEX[fine_label]
+        if cifar_version == 100:
+            set_type, coarse_label, fine_label, file_name = line.strip().split(",")
+            file_name += ".pt" if augmented_data else ".jpg"
+            set_type = f"cifar-{cifar_version}_{set_type}"
+            path_to_img_file = os.path.join(
+                img_dir_path, set_type, coarse_label, fine_label, file_name
+            )
+            enumerated_label = LABEL_TO_INDEX_CIFAR100[fine_label]
+        else:
+            set_type, fine_label, file_name = line.strip().split(",")
+            file_name += ".pt" if augmented_data else ".jpg"
+            set_type = f"cifar-{cifar_version}_{set_type}"
+            path_to_img_file = os.path.join(
+                img_dir_path, set_type, fine_label, file_name
+            )
+            enumerated_label = LABEL_TO_INDEX_CIFAR10[fine_label]
 
         # Add image and label to stored paths and labels
         all_paths.append(path_to_img_file)
         all_labels.append(enumerated_label)
-        if set_type == "cifar-100_train":
+        if set_type == f"cifar-{cifar_version}_train":
             train_val_paths.append(path_to_img_file)
             train_val_labels.append(enumerated_label)
-        elif set_type == "cifar-100_test":
+        elif set_type == f"cifar-{cifar_version}_test":
             test_paths.append(path_to_img_file)
             test_labels.append(enumerated_label)
         else:
-            raise ValueError(f"Invalid set type value in csv file (recieved: {set_type})")
+            raise ValueError(f"Invalid set type value in csv file (received: {set_type})")
         
     file.close()
 
     # If not splitting data in sets, Dataset and DataLoader objects are created and returned containing all images in the dataset (used for creating CAMs)
     if not split:
-
-        complete_dataset = CIFAR100Dataset(all_paths, all_labels, transform_type="eval", augmented_data=False)
-        complete_dataloader = DataLoader(complete_dataset, batch_size=batch_size, shuffle=False)
         
+        complete_dataset = CIFARDataset(all_paths, all_labels, transform_type="eval", augmented_data=False, cifar_version=cifar_version)
+        complete_dataloader = DataLoader(complete_dataset, batch_size=batch_size, shuffle=False)
+
         return complete_dataset, complete_dataloader
 
     # Performing split of training and validation sets
-    train_paths, val_paths, train_labels, val_labels = train_test_split(train_val_paths, train_val_labels, test_size=params["val_size"], stratify=train_val_labels if params["stratify"] else None, random_state=seed)
+    train_paths, val_paths, train_labels, val_labels = train_test_split(
+        train_val_paths,
+        train_val_labels,
+        test_size=params["val_size"],
+        stratify=train_val_labels if params["stratify"] else None,
+        random_state=seed
+    )
 
     # Asserting that sets are disjoint
     assert set(train_paths).isdisjoint(set(val_paths))
@@ -419,9 +463,9 @@ def prepare_data_cifar100(seed, augmented_data, model_explanation, split, batch_
     assert set(val_paths).isdisjoint(set(test_paths))
 
     # Creating Datasets and DataLoaders
-    train_dataset = CIFAR100Dataset(train_paths, train_labels, transform_type="train", augmented_data=augmented_data)
-    val_dataset = CIFAR100Dataset(val_paths, val_labels, transform_type="eval", augmented_data=augmented_data)
-    test_dataset = CIFAR100Dataset(test_paths, test_labels, transform_type="eval", augmented_data=augmented_data)
+    train_dataset = CIFARDataset(train_paths, train_labels, transform_type="train", augmented_data=augmented_data, cifar_version=cifar_version)
+    val_dataset = CIFARDataset(val_paths, val_labels, transform_type="eval", augmented_data=augmented_data, cifar_version=cifar_version)
+    test_dataset = CIFARDataset(test_paths, test_labels, transform_type="eval", augmented_data=augmented_data, cifar_version=cifar_version)
 
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
@@ -430,6 +474,6 @@ def prepare_data_cifar100(seed, augmented_data, model_explanation, split, batch_
     return train_dataset, val_dataset, test_dataset, train_dataloader, val_dataloader, test_dataloader
 
 if __name__ == "__main__":
-    # Downloading and storing CIFAR-100 dataset when running this file
+    # Downloading and storing CIFAR-10 and CIFAR-100 datasets when running this file
     data_root = os.path.join(os.getcwd(), "res")
-    save_cifar100_in_directory(data_root)
+    save_cifar_in_directory(data_root)
